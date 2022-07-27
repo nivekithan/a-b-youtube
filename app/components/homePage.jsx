c47b386b94a467b8aa42c4b11b5f52ba1fe7bdd3
Nivekithan S <nivekithan363@gmail.com>
2022-07-27 18:57:36 +0530

    Add js working example

---
+++ app/components/homePage.jsx
@@ -0,0 +1,3 @@
+export const HomePage = () => {
+  return <div className="bg-green-500 text-white">Hello there</div>;
+};
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,9 @@
-export default function RenderHomePage() {
-  return <div className=" bg-blue-700 text-white p-5">Hello there</div>;
+import { HomePage } from "../components/homePage";
+
+export default function Page() {
+  return (
+    <div className=" bg-blue-700 text-white p-5">
+      <HomePage />
+    </div>
+  );
 }
