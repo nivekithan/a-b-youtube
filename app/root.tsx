e46eb5a7ce33504c9e00c5148169738ab8637c1d
Nivekithan S <nivekithan363@gmail.com>
2022-07-27 18:42:57 +0530

    Remove unncessary code

--- app/root.tsx
+++ app/root.tsx
@@ -7,10 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { withEmotionCache } from "@emotion/react";
-import { unstable_useEnhancedEffect as useEnhancedEffect } from "@mui/material";
 // import theme from "./src/theme";
-import { clientStyleContext } from "./mui/clientStyleContext";
 import styles from "./styles/app.css";
 import type { LinksFunction } from "@remix-run/server-runtime";
 
@@ -19,54 +16,30 @@ interface DocumentProps {
   title?: string;
 }
 
-const Document = withEmotionCache(
-  ({ children, title }: DocumentProps, emotionCache) => {
-    const clientStyleData = React.useContext(clientStyleContext);
-
-    // Only executed on client
-    useEnhancedEffect(() => {
-      // re-link sheet container
-      emotionCache.sheet.container = document.head;
-      // re-inject tags
-      const tags = emotionCache.sheet.tags;
-      emotionCache.sheet.flush();
-      tags.forEach((tag) => {
-        // eslint-disable-next-line no-underscore-dangle
-        (emotionCache.sheet as any)._insertTag(tag);
-      });
-      // reset cache to reapply global styles
-      clientStyleData.reset();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
-          {title ? <title>{title}</title> : null}
-          <Meta />
-          <Links />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <meta
-            name="emotion-insertion-point"
-            content="emotion-insertion-point"
-          />
-        </head>
-        <body id="app">
-          {children}
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    );
-  }
-);
+const Document = ({ children, title }: DocumentProps) => {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+        {title ? <title>{title}</title> : null}
+        <Meta />
+        <Links />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </head>
+      <body id="app">
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+};
 
 export const links: LinksFunction = () => {
   return [
--- app/routes/logout.tsx
+++
@@ -1,6 +0,0 @@
-import type { LoaderFunction } from "@remix-run/server-runtime";
-import { removeUserSession } from "~/server/session.server";
-
-export const loader: LoaderFunction = async ({ request }) => {
-  return removeUserSession(request, "/");
-};
