99318be5c2380cb3ba7dd309310f8714549e7ff4
Divyansh <divyanshvermafast4@gmail.com>
2022-07-28 23:55:02 +0530

    .

--- app/calls/cards.jsx
+++ app/calls/cards.jsx
@@ -3,7 +3,6 @@ import "../styles/videoRecord.css";
 var data = [
   {
     clicks: 4000,
-    stats: `+${18}%`,
     date: "12th july 2022",
     img: "https://images.unsplash.com/photo-1658901742285-a5cba478b576?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=918&q=80",
   },
